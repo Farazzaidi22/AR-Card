19952f808b5707227154f65a88ba0e54b5ba9bae
Faraz Khalid <farazkhalid05@yahoo.com>
2020-04-26 19:31:11 +0500

    UI made

---
+++ Assets/_Scripts/BackAndExit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackAndExit : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Exited Scene");
+    }
+   
+}
---
+++ Assets/_Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+   public void AR_Card()
+   {
+       SceneManager.LoadScene("AR-Card");
+   }
+
+   public void RotateCube()
+   {
+       SceneManager.LoadScene("RotateCube");
+   }
+
+   public void Exit()
+   {
+       Application.Quit();
+       Debug.Log("Exited scene");
+   }
+}
